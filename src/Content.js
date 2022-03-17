fbbee4844021409d13f87b66b4c8c544167beaa3
messithanh2k <thanhgia10x2511@gmail.com>
2022-03-17 08:29:44 +0700

    test UI

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,7 @@
-import { useEffect, useState } from "react";
-
-const tabs = ['posts', 'comments', 'albums']
-
 function Content(){
-    const [posts, setPosts] = useState([])
-    const [type, setType] = useState('posts')
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts);
-            })
-    }, [type])
     return (
         <div>
-            {tabs.map(tab => (
-                <button
-                    key={tab}
-                    style={type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333',
-                    } : {}}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-            {posts.map(post => (
-                <li key={post.id}>{post.title || post.name}</li>
-            ))}
+            Hello anh em tôi là Thanh đây
         </div>
     )
 }
