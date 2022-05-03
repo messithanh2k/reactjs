4ab1913378c0edf6c339d1a8d5810e28e3c53557
messithanh2k <62109991+messithanh2k@users.noreply.github.com>
2022-05-03 19:33:13 +0700

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,7 @@
 function Content(){
     return (
         <div>
-            <h1>Nhớ mát tơ Q rồi đúng ko anh?</h1>
-            <img src={require('./xanh.png')} alt="Quỳnh nè" width="300" height="400"/>
+            <h1>sĩ bướm vl đcmm</h1>
         </div>
     )
 }
